fc72a7a627fcb7f9886703adb5569e7f5b5f0c86
Richard Liu <guanzhi97@gmail.com>
2023-03-20 18:21:28 -0500

    Update util.py

--- util/util.py
+++ util/util.py
@@ -76,6 +76,22 @@ class ZeroNanGrad(torch.autograd.Function):
         grad[grad != grad] = 0
         return grad
 
+class DifferentiableClamp(torch.autograd.Function):
+    """
+    In the forward pass this operation behaves like torch.clamp.
+    But in the backward pass its gradient is 1 everywhere, as if instead of clamp one had used the identity function.
+    """
+
+    @staticmethod
+    # @custom_fwd
+    def forward(ctx, input, min, max):
+        return input.clamp(min=min, max=max)
+
+    @staticmethod
+    # @custom_bwd
+    def backward(ctx, grad_output):
+        return grad_output.clone(), None, None
+
 def dclamp(input, min, max):
     """
     Like torch.clamp, but with a constant 1-gradient.
